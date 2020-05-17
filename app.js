3f00171b97a54cb9e0c5d3d01074bf01b80444ed
unknown <joe_cooler@msn.com>
2020-05-17 10:27:35 -0500

    app now updates database based on amount purchased and displays total price

--- app.js
+++ app.js
@@ -3,49 +3,51 @@ var inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
     host: "localhost",
-  
+
     // Your port; if not 3306
     port: 3306,
-  
+
     // Your username
     user: "root",
-  
+
     // Your password
     password: "Scat1982Pack",
     database: "bamazon_db"
-  });
-  
-  connection.connect(function(err) {
+});
+
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
+
     displayStock();
-    setTimeout(function() {
+    setTimeout(function () {
         console.log('\n');
         displayMenu();
     }, 100);
-    connection.end();
-  })
 
-  
+    //connection.end();
+})
 
-     function displayStock() {
-        connection.query('select * from stock', function (err, res) {
-            if (err) throw err;
-            res.forEach(e => {
-                console.log(`${e.item_id} | ${e.product_name} | ${e.department_name} | $${e.price} | Qty: ${e.stock_quantity}`);
-                
-            })
+
+
+function displayStock() {
+    connection.query('select * from stock', function (err, res) {
+        if (err) throw err;
+        res.forEach(e => {
+            console.log(`${e.item_id} | ${e.product_name} | ${e.department_name} | $${e.price} | Qty: ${e.stock_quantity}`);
+
         })
-    }
+    })
+}
 
-    function displayMenu () {
-      
-        inquirer
+function displayMenu() {
+
+    inquirer
         .prompt([{
             name: 'itemID',
             type: 'input',
             message: 'Enter ID of the product you wish you purchase: ',
-    
+
         },
         {
             name: 'amount',
@@ -55,15 +57,25 @@ var connection = mysql.createConnection({
             /* Pass your questions in here */
         ])
         .then(answers => {
-            console.log(answers.itemID);
-            console.log(answers.amount);
-            // Use user feedback for... whatever!!
-        })
-        .catch(error => {
-            if (error.isTtyError) {
-                // Prompt couldn't be rendered in the current environment
-            } else {
-                // Something else when wrong
+            if (!answers.itemID || !answers.amount){
+                console.log('Please enter a valid item id and quantity.');
+                connection.end();
+                return;
             }
-        });
-    }
+            connection.query(`select * from stock where item_id=${answers.itemID}`, function (err, res) {
+                if (err) throw err;
+                if (answers.amount > res[0].stock_quantity) {
+                    console.log('Not enough stock to fulfill order. Please try again.');
+                    connection.end();
+                    return;
+                }
+                let qty = res[0].stock_quantity - answers.amount;
+                let total = res[0].price * answers.amount;
+                connection.query(`update stock set stock_quantity=${qty} where item_id=${answers.itemID}`, function (err, res) {
+                    if (err) throw err;
+                    console.log(`Order has been placed. Your total is $${total}. Thank you for shopping at Bamazon.`);
+                    connection.end();
+                });
+            })
+            }
+        )}
